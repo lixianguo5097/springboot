9e825e3495af323ae6a08f7f48f2e7ab8bf6ba8f
李先国 <ied_lxg@gdhqx.com>
2019-12-10 17:39:52 +0800

    删除一些无用的引入

--- springboot-logs/src/main/java/com/lxg/config/WebLogAspect.java
+++ springboot-logs/src/main/java/com/lxg/config/WebLogAspect.java
@@ -1,13 +1,10 @@
 package com.lxg.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
