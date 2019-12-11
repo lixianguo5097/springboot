4a4a8a8a4f42f42c4c4f864aa2f394fda1703c9d
李先国 <ied_lxg@gdhqx.com>
2019-12-11 17:16:13 +0800

    添加了一些注释

--- springboot-swagger/src/main/java/com/lxg/config/Swagger2Config.java
+++ springboot-swagger/src/main/java/com/lxg/config/Swagger2Config.java
@@ -17,12 +17,12 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class Swagger2Config {
-
     @Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
+                //定义扫描接口的包
                 .apis(RequestHandlerSelectors.basePackage("com.lxg.controller"))
                 .paths(PathSelectors.any())
                 .build();
@@ -30,12 +30,15 @@ public class Swagger2Config {
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
+                //定义界面标题
                 .title("Spring Boot中使用Swagger2构建RESTful APIs")
+                //定义界面描述
                 .description("swagger2-文档构建利器")
                 .termsOfServiceUrl("https://www.lxgblog.com/")
+                //作者
                 .contact("李先国")
+                //版本
                 .version("1.0")
                 .build();
     }
-
 }
