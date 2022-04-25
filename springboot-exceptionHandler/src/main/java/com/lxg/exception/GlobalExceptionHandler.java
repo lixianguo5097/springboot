61bae0fdb8169c6f9a0fe2ce7e31c102fa2074ec
李先国 <lixianguo@gdhqx.com>
2022-04-25 14:42:36 +0800

    修改 增加HttpResponseCode

--- springboot-exceptionHandler/src/main/java/com/lxg/exception/GlobalExceptionHandler.java
+++ springboot-exceptionHandler/src/main/java/com/lxg/exception/GlobalExceptionHandler.java
@@ -6,9 +6,11 @@ import com.lxg.common.Result;
 import com.lxg.common.ResultCode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
@@ -29,6 +31,7 @@ import java.util.List;
  * @date 2019-11-05
  */
 @ControllerAdvice
+@ResponseStatus(code= HttpStatus.INTERNAL_SERVER_ERROR)
 public class GlobalExceptionHandler extends WebMvcConfigurerAdapter {
     private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
