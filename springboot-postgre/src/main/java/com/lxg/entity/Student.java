c562bc63ce5429dacf187a9fc6e057c1a23d7682
李先国 <lixianguo@gdhqx.com>
2022-08-15 14:20:37 +0800

    修改 springboot+postgresql模块整合jsonArray类型字段

--- springboot-postgre/src/main/java/com/lxg/entity/Student.java
+++ springboot-postgre/src/main/java/com/lxg/entity/Student.java
@@ -1,11 +1,13 @@
 package com.lxg.entity;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.lxg.handler.ArrayTypeHandler;
 import com.lxg.handler.JSONTypeHandler;
+import com.lxg.handler.JsonArrayTypeHandler;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
@@ -30,11 +32,14 @@ public class Student implements Serializable {
     private static final long serialVersionUID = 1L;
     @TableId
     String id;
-    //数据库类型为json
+    //数据库类型为jsonb
     @TableField(value = "detail", jdbcType = JdbcType.JAVA_OBJECT, typeHandler = JSONTypeHandler.class)
     JSONObject detail;
     //数据库类型为int
     @TableField(value = "scores", jdbcType = JdbcType.ARRAY, typeHandler = ArrayTypeHandler.class)
     Integer[] scores;
+    //数据库类型为jsonb
+    @TableField(value = "school", jdbcType = JdbcType.JAVA_OBJECT, typeHandler = JsonArrayTypeHandler.class)
+    JSONArray school;
 
 }
--- springboot-postgre/src/main/java/com/lxg/sql/public.sql
+++ springboot-postgre/src/main/java/com/lxg/sql/public.sql
@@ -12,7 +12,7 @@
  Target Server Version : 140004
  File Encoding         : 65001
 
- Date: 15/08/2022 13:50:38
+ Date: 15/08/2022 14:19:16
 */
 
 
@@ -20,8 +20,8 @@
 -- Sequence structure for User_id_seq
 -- ----------------------------
 DROP SEQUENCE IF EXISTS "public"."User_id_seq";
-CREATE SEQUENCE "public"."User_id_seq" 
-INCREMENT 1
+CREATE SEQUENCE "public"."User_id_seq"
+    INCREMENT 1
 MINVALUE  1
 MAXVALUE 2147483647
 START 1
@@ -32,26 +32,27 @@ CACHE 1;
 -- ----------------------------
 DROP TABLE IF EXISTS "public"."student";
 CREATE TABLE "public"."student" (
-  "id" varchar COLLATE "pg_catalog"."default",
-  "scores" int4[],
-  "detail" jsonb
+                                    "id" varchar COLLATE "pg_catalog"."default",
+                                    "scores" int4[],
+                                    "detail" jsonb,
+                                    "school" jsonb
 )
 ;
 
 -- ----------------------------
 -- Records of student
 -- ----------------------------
-INSERT INTO "public"."student" VALUES ('1559054351975088129', '{99,72,88}', '{"姓名": "李四", "年龄": "23"}');
-INSERT INTO "public"."student" VALUES ('1559054201080807426', '{97,108,103}', '{"姓名": "张三", "年龄": "21"}');
+INSERT INTO "public"."student" VALUES ('1559054351975088129', '{99,72,88}', '{"姓名": "李四", "年龄": "23"}', '[{"小学": "广州小学"}, {"初中": "广州初中"}, {"高中": "广州高中"}]');
+INSERT INTO "public"."student" VALUES ('1559054201080807426', '{97,108,103}', '{"姓名": "张三", "年龄": "21"}', '[{"小学": "深圳小学"}, {"初中": "深圳初中"}, {"高中": "深圳高中"}]');
 
 -- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS "public"."user";
 CREATE TABLE "public"."user" (
-  "id" varchar(32) COLLATE "pg_catalog"."default" NOT NULL DEFAULT nextval('"User_id_seq"'::regclass),
-  "username" varchar(20) COLLATE "pg_catalog"."default",
-  "password" varchar(20) COLLATE "pg_catalog"."default"
+                                 "id" varchar(32) COLLATE "pg_catalog"."default" NOT NULL DEFAULT nextval('"User_id_seq"'::regclass),
+                                 "username" varchar(20) COLLATE "pg_catalog"."default",
+                                 "password" varchar(20) COLLATE "pg_catalog"."default"
 )
 ;
 
@@ -64,7 +65,7 @@ INSERT INTO "public"."user" VALUES ('1557985624521191426', '张三', 'abc');
 -- Alter sequences owned by
 -- ----------------------------
 ALTER SEQUENCE "public"."User_id_seq"
-OWNED BY "public"."user"."id";
+    OWNED BY "public"."user"."id";
 SELECT setval('"public"."User_id_seq"', 2, false);
 
 -- ----------------------------
